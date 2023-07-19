7ea9e963b3ccda500e3c345a31897d335dd6bacc
bob <gaston.skamlec@beclevercorp.com>
2023-07-19 16:58:36 -0300

    Bubble sort implementation

---
+++ Algorithms/Sort/BubbleSort.js
@@ -0,0 +1,21 @@
+const bubbleSearch = (array) =>{
+     debugger
+    for(let i = 0; i < array.length; ++i)
+    {
+        for (let j = 0; j < array.length - 1 - i; ++j) 
+        {
+
+            if(array[j] > array[j+1]){
+                let tmp = array[j];
+                array[j] = array[j+1];
+                array[j+1] = tmp;
+            }
+
+        }
+    }
+
+}
+
+let example = [78,1,45,35,144,2,47,96,38,74,12,25,96,54,21];
+
+bubbleSearch(example);
